b32ec870005b00928c70f317f110add7ce07caac
Dragan Pepic <d.pepic@hotmail.com>
2020-10-15 22:08:52 +0200

    sdfsdf

--- BlazorSuviIdent/Shared/Osoba.cs
+++ BlazorSuviIdent/Shared/Osoba.cs
@@ -9,5 +9,12 @@ namespace BlazorSuviIdent.Shared
 		public string ID { get; set; } = Guid.NewGuid().ToString();
 		public string Ime { get; set; }
 		public string Prezime { get; set; }
+
+		public void Deconstruct(out string ID, out string Ime, out string Prezime)
+		{
+			ID = this.ID;
+			Ime = this.Ime;
+			Prezime = this.Prezime;
+		}
 	}
 }
